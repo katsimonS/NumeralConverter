cdb02ec0d36f1397ef6a18777c25438080fdc9a9
Serhii Katsimon <jora.proforg@gmail.com>
2022-07-11 09:57:11 +0300

    DecimalToBinary

--- NumeralConverter/Program.cs
+++ NumeralConverter/Program.cs
@@ -1,12 +1,29 @@
 ﻿using System;
+using System.Text;
 
 namespace NumeralConverter
 {
     class Program
     {
+        static string DecimalToBinary(int decimalNumber)
+        {
+            var binaryNumber = string.Empty;
+            while (decimalNumber > 0)
+            {
+                binaryNumber = (decimalNumber % 2) + binaryNumber;
+                decimalNumber /= 2;
+            }
+
+            return binaryNumber;
+        }
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.OutputEncoding = Encoding.UTF8;
+            Console.Write("Введите число в десятичной системе: ");
+            var decNum = int.Parse(Console.ReadLine());
+            var binNum = DecimalToBinary(decNum);
+            Console.WriteLine("{0} => {1}", decNum, binNum);
+            Console.ReadLine();
         }
     }
 }
